5c92a022124c1e9125a42bfa3b7213fff1e1518b
Rahman Rejepov <rahmanrejepov777@gmail.com>
2023-09-20 15:28:09 +0300

    state hook for sign up page

--- client/src/components/signUp.js
+++ client/src/components/signUp.js
@@ -1,11 +1,30 @@
 import {Link, useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 function SignUp() {
     const navigate = useNavigate();
+    const [state, setState] = useState({lastName:"", firstName:"", email:"", password:""});
 
     const handleRegister = (e) => {
         e.preventDefault();
+        console.log(state);
         navigate("/register");
+    }
+    const handleLastName = (e) => {
+        e.preventDefault();
+        setState({...state, lastName: e.target.value});
+    }
+    const handleFirstName = (e) => {
+        e.preventDefault();
+        setState({...state, firstName: e.target.value});
+    }
+    const handleEmail = (e) => {
+        e.preventDefault();
+        setState({...state, email: e.target.value});
+    }
+    const handlePassword = (e) => {
+        e.preventDefault();
+        setState({...state, password: e.target.value});
     }
 
     return (
@@ -14,16 +33,16 @@ function SignUp() {
                 <h1>Register</h1>
                 <div className="content">
                     <div className="input-field">
-                        <input type="last-name" placeholder="Last Name" autoComplete="nope"/>
+                        <input type="last-name" placeholder="Last Name" autoComplete="nope" onChange={handleLastName}/>
                     </div>
                     <div className="input-field">
-                        <input type="first-name" placeholder="First Name" autoComplete="nope"/>
+                        <input type="first-name" placeholder="First Name" autoComplete="nope" onChange={handleFirstName}/>
                     </div>
                     <div className="input-field">
-                        <input type="email" placeholder="Email" autoComplete="nope"/>
+                        <input type="email" placeholder="Email" autoComplete="nope" onChange={handleEmail}/>
                     </div>
                     <div className="input-field">
-                        <input type="password" placeholder="Password" autoComplete="new-password"/>
+                        <input type="password" placeholder="Password" autoComplete="new-password" onChange={handlePassword}/>
                     </div>
                     <Link to={"/login"} className="link">Have an account?</Link>
                 </div>
