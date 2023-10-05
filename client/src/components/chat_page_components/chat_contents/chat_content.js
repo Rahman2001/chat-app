3ce97a76814071b37580b162ed8297e198a4bc5d
Rahman Rejepov <rahmanrejepov777@gmail.com>
2023-10-05 17:05:45 +0300

    send function added

--- client/src/components/chat_page_components/chat_contents/chat_content.js
+++ client/src/components/chat_page_components/chat_contents/chat_content.js
@@ -1,7 +1,30 @@
 import MessageGenerator from "./message_generator";
+import {useContext, useState} from "react";
+import {UserContext} from "../../../App";
 import Message from "./message";
 
 function ChatContent() {
+    const [inputState, setInputState] = useState('');
+    const [message, setMessage] = useState([]);
+    const [lastTime, setLastTime] = useState("");
+    const userDetails = useContext(UserContext);
+    const handleChange = (e) => {
+        e.preventDefault();
+        const text = e.target.value;
+        setInputState(text);
+    }
+    const handleClick = (e) => {
+        e.preventDefault();
+        let date = new Date();
+        let currentTime = date.getHours() + ":" + date.getMinutes();
+        if(lastTime === currentTime) {
+            currentTime = "";
+        }
+        console.log(currentTime);
+        setLastTime(currentTime);
+        setMessage([...message, inputState]);
+        setInputState('');
+    }
     return (
         <section className="chat">
             <div className="header-chat">
@@ -18,27 +41,15 @@ function ChatContent() {
                         </div>
                         <p className="text"> Hi, how are you ? </p>
                     </div>
-                    <div className="message text-only">
-                        <p className="text"> What are you doing tonight ? Want to go take a drink ?</p>
-                    </div>
-                    <p className="time"> 14h58</p>
-                    <div className="message text-only">
-                        <div className="response">
-                            <p className="text"> Hey Megan ! It's been a while 😃</p>
-                        </div>
-                    </div>
-                    <div className="message text-only">
-                        <div className="response">
-                            <p className="text"> When can we meet ?</p>
-                        </div>
-                    </div>
-                    <MessageGenerator/>
+                    {message.map((mes) => (
+                        <Message message={mes} sender={userDetails} time={lastTime}/>
+                    ))}
                 </div>
             </div>
             <div className="footer-chat">
                 <i className="icon fa fa-smile-o clickable" style={{fontSize:'25pt', ariaHidden:true}}></i>
-                <input type="text" className="write-message" placeholder="Type your message here"/>
-                <i className="icon send fa fa-paper-plane-o clickable" aria-hidden="true"></i>
+                <input type="text" className="write-message" placeholder="Type your message here" value={inputState} onChange={handleChange}/>
+                <button type={"submit"} className={"icon send fa fa-paper-plane-o clickable"} aria-hidden={true} onClick={handleClick}/>
             </div>
         </section>
     );
--- client/src/components/chat_page_components/chat_contents/message.js
+++ client/src/components/chat_page_components/chat_contents/message.js
@@ -1,12 +1,30 @@
-function Message({message, sender}) {
-    return (
-        <div className="message">
-            <div className="photo"
-                 style={{backgroundImage: "url(\"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80\")"}}>
-                <div className="online"></div>
+import {useContext} from "react";
+import {UserContext} from "../../../App";
+
+function Message({message, sender, time, messageId}) {
+    const userDetails = useContext(UserContext);
+    if(message !== '') {
+        return userDetails.email === sender.email ?(
+            <div className="first-party-message" id={messageId}>
+                <div className="message text-only">
+                    <div className="response">
+                        <p className="text">{message}</p>
+                    </div>
+                </div>
+                {time !== "" && <p className="time">{time}</p>}
             </div>
-            <p className="text"> 9 pm at the bar if possible 😳</p>
-        </div>
-    );
+        ) : (
+            <div className="second-party-message">
+                <div className="message">
+                    <div className="photo"
+                         style={{backgroundImage: "url(\"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80\")"}}>
+                        <div className="online"></div>
+                    </div>
+                    <p className="text">{message}</p>
+                </div>
+                {time !== "" && <p className="time">{time}</p>}
+            </div>
+        );
+    }
 }
 export default Message;
